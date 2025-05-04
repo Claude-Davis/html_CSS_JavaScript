fe6ab00d1b76c28d8b00002caf94983ff550a8da
Claudé Davis <claudedavis2024@gmail.com>
2025-05-04 19:10:25 -0400

    Create Mathematics.js
    notes for mathematical operators

---
+++ Intro to JavaScript/Mathematics.js
@@ -0,0 +1,21 @@
+let x = 40;
+let y = 5;
+let answer;
+
+//subtraction
+answer = x-y; //answer = 35
+
+//addition
+answer = x+y; //answer = 45
+
+//multiplication
+answer = x*y; //answer = 200
+
+//division
+answer = x/y; //answer = 8
+
+//modulus
+answer = x%y; //answer = 0
+
+//exponentiation
+answer = x**y; //answer = 102,400,000
