5e423997bd3f4d4148559678c8956abc444fe03c
Claude Davis <claudedavis2024@gmail.com>
2025-06-24 12:48:06 -0400

    updated speed of background

--- Udemy_GameDev_Course/with Images/script.js
+++ Udemy_GameDev_Course/with Images/script.js
@@ -168,9 +168,9 @@ window.addEventListener('load', function(){ //LOAD EVENT: executes when the whol
             this.image3 = document.getElementById('layer3');
             this.image4 = document.getElementById('layer4');
             this.layer1 = new Layer(this.game, this.image1, 1)
-            this.layer2 = new Layer(this.game, this.image2, 1)
-            this.layer3 = new Layer(this.game, this.image3, 1)
-            this.layer4 = new Layer(this.game, this.image4, 1)
+            this.layer2 = new Layer(this.game, this.image2, 0.3)
+            this.layer3 = new Layer(this.game, this.image3, 0.3)
+            this.layer4 = new Layer(this.game, this.image4, 0.5)
             this.layers = [this.layer1, this.layer2, this.layer3, this.layer4];
 
         }
